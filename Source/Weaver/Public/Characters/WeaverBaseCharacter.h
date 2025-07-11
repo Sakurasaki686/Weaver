62dd5549ad778b5d2fb30392d2380f8f8025205a
sakurasaki <2252523310@qq.com>
2025-07-11 14:01:04 +0800

    Initial commit

---
+++ Source/Weaver/Public/Characters/WeaverBaseCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "WeaverBaseCharacter.generated.h"
+
+UCLASS()
+class WEAVER_API AWeaverBaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AWeaverBaseCharacter();
+
+protected:
+
+public:	
+};
