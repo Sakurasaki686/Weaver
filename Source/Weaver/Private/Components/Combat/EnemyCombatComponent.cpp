0aba28ad6669bd2fca7e23315583d5c182858d9f
sakurasaki <2252523310@qq.com>
2025-07-25 00:50:15 +0800

    ...

--- Source/Weaver/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Weaver/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -3,3 +3,13 @@
 
 #include "Components/Combat/EnemyCombatComponent.h"
 
+void UEnemyCombatComponent::SetSelectedElement(UDataAsset_ElementBase* InElement)
+{
+	SelectedElement = InElement;
+}
+
+void UEnemyCombatComponent::SetSelectedEffect(UDataAsset_EffectBase* InEffect)
+{
+	SelectedEffect = InEffect;
+}
+
--- Source/Weaver/Public/Components/Combat/EnemyCombatComponent.h
+++ Source/Weaver/Public/Components/Combat/EnemyCombatComponent.h
@@ -6,6 +6,8 @@
 #include "Components/Combat/PawnCombatComponent.h"
 #include "EnemyCombatComponent.generated.h"
 
+class UDataAsset_EffectBase;
+class UDataAsset_ElementBase;
 /**
  * 
  */
@@ -13,5 +15,24 @@ UCLASS()
 class WEAVER_API UEnemyCombatComponent : public UPawnCombatComponent
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Weaver|Spell Alchemist")
+	TObjectPtr<UDataAsset_ElementBase> SelectedElement;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weaver|Spell Alchemist")
+	TObjectPtr<UDataAsset_EffectBase> SelectedEffect;
+
+public:
+	UFUNCTION(BlueprintPure, Category = "Weaver|Spell Alchemist")
+	UDataAsset_ElementBase* GetSelectedElement() const { return SelectedElement; }
+
+	UFUNCTION(BlueprintPure, Category = "Weaver|Spell Alchemist")
+	UDataAsset_EffectBase* GetSelectedEffect() const { return SelectedEffect; }
+
+	UFUNCTION(BlueprintCallable, Category = "Weaver|Spell Alchemist")
+	void SetSelectedElement(UDataAsset_ElementBase* InElement);
+
+	UFUNCTION(BlueprintCallable, Category = "Weaver|Spell Alchemist")
+	void SetSelectedEffect(UDataAsset_EffectBase* InEffect);
 };
